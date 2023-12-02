4176747496fe60ba4db43f7701a9b130274b25f1
Mauricio Oliveira <mauricio.oliveira8583@gmail.com>
2023-12-02 19:57:52 -0300

    Feature: Animation on the logo in the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import './navbar.scss'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='wrapper'>
-            <span>Mauricio Oliveira</span>
+            <motion.span initial={{opacity:0, scale:0.5}} 
+            animate={{opacity:1, scale: 1}}
+            transition={{duration:1}} >Mauricio Oliveira</motion.span>
             <div className='social'>
-                <a href='https://www.linkedin.com/in/mauricio-oliveira-a1a0aa164/'><img src='/linkedin.png' alt='LinkedIn' /></a>
-                <a href='https://github.com/Mauricio8583'><img src='/github.png' alt='Github' /></a>
-                <a href='https://www.instagram.com/mauricio8583'><img src='/instagram.png' alt='Instagram' /></a>
-                <a href='https://www.facebook.com/mauricio.oliveira.54772728'><img src='/facebook.png' alt='Facebook' /></a>                
+                <a href='https://www.linkedin.com/in/mauricio-oliveira-a1a0aa164/' target='_blank'><img src='/linkedin.png' alt='LinkedIn' /></a>
+                <a href='https://github.com/Mauricio8583' target='_blank'><img src='/github.png' alt='Github' /></a>
+                <a href='https://www.instagram.com/mauricio8583' target='_blank'><img src='/instagram.png' alt='Instagram' /></a>
+                <a href='https://www.facebook.com/mauricio.oliveira.54772728' target='_blank'><img src='/facebook.png' alt='Facebook' /></a>                
             </div>
         </div>
     </div>
