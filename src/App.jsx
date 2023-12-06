5ada49dae82992f392e4f5e045afc95dde9db066
Mauricio Oliveira <mauricio.oliveira8583@gmail.com>
2023-12-05 22:19:55 -0300

    Feature: Creating Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss"
 import Navbar from "./components/navbar/Navbar"
 import Parallax from "./components/parallax/Parallax"
 import Profile from "./components/profile/Profile"
+import Skills from "./components/skills/Skills"
 
 function App() {
   
@@ -12,7 +13,7 @@ function App() {
         <Profile />
       </section>
       <section><Parallax type='skills' /></section>
-      <section id="Habilidades"><a href="">Habilidades</a></section>
+      <section id="Habilidades"><Skills /></section>
       <section><Parallax type='portfolio' /></section>
       <section id="Portfolio"><a href="">Portfolio</a></section>
       <section id="Contato"><a href="">Contato</a></section>
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,74 @@
+import { useRef } from 'react'
+import './skills.scss'
+import { motion, useInView } from 'framer-motion'
+
+const Skills = () => {
+
+   const ref = useRef();
+
+   const isInView = useInView(ref, {margin: '-100px'});
+
+   const variants = {
+    initial: {
+        x: -500,
+        y: 100,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    }
+   }
+
+  return (
+    <motion.div className='skills' variants={variants} initial='initial' animate={isInView && 'animate'} ref={ref}>
+        <motion.div className='textContainer' variants={variants}>
+            <p>Desenvolvedor Júnior <br />
+            Estudante de Tecnologia</p>
+            <hr />
+        </motion.div>
+        <motion.div className='titleContainer' variants={variants}>
+            <div className='title'>
+                <h1><motion.b whileHover={{color: 'orange'}}>Algumas</motion.b> das</h1>
+            </div>
+            <div className='title'>
+                <h1><motion.b whileHover={{color: 'orange'}}>Tecnologias</motion.b> que uso</h1>
+                <button>O que faço</button>
+            </div>
+        </motion.div>
+        <motion.div className='listContainer' variants={variants}>
+            <motion.div className='box' whileHover={{background: 'lightgray', color: 'black'}}>
+                <h2>Javascript</h2>
+                <p>Já trabalhei profissionalmente com essa linguagem, tenho contato com Javascript desde de 2014, 
+                também já desenenvolvi projetos pessoais com ela.
+                </p>
+            </motion.div>
+            <motion.div className='box' whileHover={{background: 'lightgray', color: 'black'}}>
+                <h2>ReactJs</h2>
+                <p>Já desenvolvi projetos complexos com este framework, ReactJs é usado neste portfolio, 
+                    possuo contato com React desde 2020.
+                </p>
+            </motion.div>
+            <motion.div className='box' whileHover={{background: 'lightgray', color: 'black'}}>
+                <h2>Python</h2>
+                <p>Tenho contato com Python desde 2019, já desenvolvi projetos complexos integrado com React no frontEnd, 
+                    utilizei Python em meu TCC.
+                </p>
+            </motion.div>
+            <motion.div className='box' whileHover={{background: 'lightgray', color: 'black'}}>
+                <h2>SQL</h2>
+                <p>Utilizei SQL para trabalhar com banco de dados em projetos complexos sendo a tecnologia de 
+                    banco de dados relacional que mais tenho familiaridade, tenho contato com esta tecnologia desde 2014
+                </p>
+            </motion.div>
+        </motion.div>
+    </motion.div>
+  )
+}
+
+export default Skills
